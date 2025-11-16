76fcda42af54b806d49b35705f5012491f20d5fd
Your Name <aniket56614@gmail.com>
2025-11-16 18:42:10 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running ar port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=> {
+    console.log("mongo db coonection failed !!",err)
+
+}) 
+
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error{
+    constructor(
+        statuscode,
+        message = "Something went wrong", 
+        errors = [], 
+        statck =""
+
+    ){
+        super(message)
+        this.statusCode =statuscode
+        this.data =null
+        this.message =message
+        TouchList.sucess =false
+        this.errors = errors
+
+        if(statck){
+            this.stack = stack
+
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+export {ApiError}
